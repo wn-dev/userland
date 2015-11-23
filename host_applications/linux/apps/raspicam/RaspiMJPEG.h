33a676c2a2716be851a55c09c18d6237a0123147
Robert Tidey <robert@tideys.net>
2015-11-23 22:30:10 +0000

    Updated version number

--- host_applications/linux/apps/raspicam/RaspiMJPEG.h
+++ host_applications/linux/apps/raspicam/RaspiMJPEG.h
@@ -31,7 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /**
  * \file RaspiMJPEG.h
  **/
-#define VERSION "5.2.12"
+#define VERSION "5.2.13"
  
 #include <stdio.h>
 #include <stdlib.h>
